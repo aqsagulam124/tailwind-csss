aea06d55419c451ba7a5e3611b144e919da6a958
Aqsa <aqsagulamhasan@gmail.com>
2025-01-06 13:55:55 +0500

    Add files via upload

---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <footer>
+    <p>&copy; 2024 watch world. All Rights Reserved.</p>
+</footer>
+
+  )
+}
+
+export default footer
